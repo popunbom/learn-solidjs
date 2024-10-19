b36604fadb27d837d2f6ca7749e098e32ccddc97
popunbom <28749104+popunbom@users.noreply.github.com>
2024-10-19 16:54:25 +0900

    Clean-up

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 
-import './index.css'
 import App from '@/pages/App'
+
+import './index.css'
 
 const root = document.getElementById('root')
 
