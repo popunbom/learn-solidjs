b218a52e8d7e42f4af8ab7b95cbe86ab25abd39b
popunbom <28749104+popunbom@users.noreply.github.com>
2024-10-20 21:02:02 +0900

    Check qualify

--- src/states/board.ts
+++ src/states/board.ts
@@ -27,21 +27,33 @@ export function updateBoard(i: number, j: number, v: number) {
   newBoard[i][j] = { state: 'defined', value: v };
 
   // Scan
-  const isFinished = (cells: Cell[]): cells is ValueDefinedCell[] =>
+  const isFilled = (cells: Cell[]): cells is ValueDefinedCell[] =>
     cells.every(c => ['fixed', 'defined', 'finished'].includes(c.state));
+  const isQualified = (cells: ValueDefinedCell[]): boolean => {
+    const values = cells.map(c => c.value);
+    return new Set(values).size === values.length;
+  }
 
   // Scan row
   const cellInRow = Array.from({ length: boardSize }, (_, k) => newBoard[i][k]);
-  if (isFinished(cellInRow)) {
+  if (isFilled(cellInRow)) {
     console.log(`Row ${i} is finished`);
-    cellInRow.forEach(c => c.state = 'finished');
+    if (isQualified(cellInRow)) {
+      cellInRow.forEach(c => c.state = 'finished');
+    } else {
+      cellInRow.forEach(c => c.state = 'error');
+    }
   }
 
   // Scan column
   const cellInColumn = Array.from({ length: boardSize }, (_, k) => newBoard[k][j]);
-  if (isFinished(cellInColumn)) {
+  if (isFilled(cellInColumn)) {
     console.log(`Column ${j} is finished`);
-    cellInColumn.forEach(c => c.state = 'finished');
+    if (isQualified(cellInColumn)) {
+      cellInColumn.forEach(c => c.state = 'finished');
+    } else {
+      cellInColumn.forEach(c => c.state = 'error');
+    }
   }
 
   // Scan box
@@ -51,9 +63,13 @@ export function updateBoard(i: number, j: number, v: number) {
     Array.from({ length: boxSize }).map((_, l) =>
       newBoard[(boxI * boxSize) + k][(boxJ * boxSize) + l])
   ).flat();
-  if (isFinished(cellInBox)) {
+  if (isFilled(cellInBox)) {
     console.log(`Box (${boxI}, ${boxJ}) is finished`);
-    cellInBox.forEach(c => c.state = 'finished');
+    if (isQualified(cellInBox)) {
+      cellInBox.forEach(c => c.state = 'finished');
+    } else {
+      cellInBox.forEach(c => c.state = 'error');
+    }
   }
 
   setBoard(newBoard);
